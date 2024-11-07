1ab0e173dea4c2ca1469355c228e1872842fc19e
Jasong237 <jasonguo4635@gmail.com>
2024-11-06 20:28:18 -0500

    54thyrgr

---
+++ app/JS/dom.js
@@ -0,0 +1,10 @@
+const DOMSelectors = {
+  form: document.querySelector("form"),
+  container: document.querySelector(".container"),
+  products: document.querySelector(".products"),
+  name: document.querySelector("name"),
+  price: document.querySelector(".price"),
+  category: document.querySelector(".category"),
+};
+
+export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -2,15 +2,20 @@ import "../css/style.css";
 import { DOMSelectors } from "../JS/dom.js";
 import { products } from "../JS/products.js";
 
-products.forEach((product) => {
-  create();
-});
+function displayProducts() {
+  products.forEach((product) => {
+    addCard(product);
+  });
+}
 
-function create() {
+displayProducts();
+
+function addCard(product) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card"> 
-      <h2 class="card-name">   ${product.name} </h2>
+      <h2 class="product-name"> ${product.name} </h2>
+      <h2 class="product-price"> Price: $${product.price} </h2>
     </div>`
   );
 }
