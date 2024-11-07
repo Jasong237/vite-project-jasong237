d14a62f9d52291398a37837c23d67aa6a0013726
Jasong237 <jasonguo4635@gmail.com>
2024-11-07 13:55:54 -0500

    werfewrtfegf

--- app/JS/main.js
+++ app/JS/main.js
@@ -2,24 +2,34 @@ import "../css/style.css";
 import { DOMSelectors } from "../JS/dom.js";
 import { products } from "../JS/products.js";
 
-function displayProducts() {
+function addProducts() {
   products.forEach((product) => {
     addCard(product);
   });
 }
 
-displayProducts();
-
 function addCard(product) {
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class="card"> 
-      <h2 class="product-name"> ${product.name} </h2>
+      <h2 class="product-name" id="${product.name}"> ${product.name} </h2>
       <h2 class="product-price"> Price: $${product.price} </h2>
     </div>`
   );
 }
 
+addProducts();
+
+DOMSelectors.button.addEventListener("click", click);
+
+function click(event) {
+  event.preventDefault();
+  document.querySelector(".container").innerHTML = "";
+
+  addProducts();
+}
 // cd app
 // npm install
 // npm run dev
+
+//last day tuesday!
--- app/JS/products.js
+++ app/JS/products.js
@@ -3,18 +3,24 @@ const products = [
   { name: "PV", price: 60, category: "Faces" },
   { name: "SKOTN", price: 300, category: "Hats" },
   { name: "GTH", price: 20, category: "Hats" },
-  { name: "ASTRA", price: 1000, category: "Hats, Rares" },
+  { name: "ASTRA", price: 1000, category: "Hats" || "Rares" },
   { name: "BBM", price: 55, category: "Faces" },
   { name: "PQOTN", price: 180, category: "Hats" },
-  { name: "MBSTF", price: 550, category: "Hats, Rares" },
+  { name: "MBSTF", price: 550, category: "Hats" || "Rares" },
   { name: "YUM", price: 350, category: "Faces" },
   { name: "CWHP", price: 500, category: "Hats" },
-  { name: "GSTF", price: 600, category: "Hats, Rares" },
+  { name: "GSTF", price: 600, category: "Hats" || "Rares" },
   { name: "FRZN", price: 550, category: "Hats" },
-  { name: "RT", price: 800, category: "Faces, Rares" },
+  { name: "RT", price: 800, category: "Faces" || "Rares" },
   { name: "RDC", price: 50, category: "Hats" },
-  { name: "EOA", price: 500, category: "Faces, Rares" },
+  { name: "EOA", price: 500, category: "Faces" || "Rares" },
   { name: "CHICK", price: 300, category: "Hats" },
 ];
 
+const categories = ["Hats", "Rares", "Faces"];
+const rares = products.filter((product) => product.category === "Rares");
+const hats = products.filter((product) => product.category === "Hats");
+const faces = products.filter((product) => product.category === "Faces");
 export { products };
+
+console.log("Rares", rares);
